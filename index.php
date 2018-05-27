fae51c7aad2b069ee78feaaff2c9efd091f0d348
josueadame <josueadame28@gmail.com>
2018-05-27 18:00:48 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<HTML>
+<HEAD>
+<title> INDEX </TITLE>
+<TITLE> complex </TITLE>
+<DIV STYLE="TEXT-ALIGN: CENTER;"><font face="cooper" size="15" color="white"size="5"><BIG>COMPLEXXX&reg</BIG></DIV>
+<hr align="center" size="1"><br>
+<BODY bgcolor=black>
+
+<TABLE BORDER="2" WIDTH="100%">
+
+<TR><TD><p align="center"><font color="white"size="4"><A HREF="https://josueadame28.000webhostapp.com/UA.HTML">Tenis UNDER ARMOUR</TR></TD>
+</P>
+
+<TR><TD><p align="center"><font color="white"size="4"><A HREF="https://josueadame28.000webhostapp.com/NIKE.html">Tenis Nike</TR></TD>
+</P>
+
+<TR><TD><p align="center"><font color="white"size="4"><A HREF="https://josueadame28.000webhostapp.com/ADIDAS.HTML">Tenis Adidas</A></TR></TD>
+</P>
+
+</TABLE>
+<hr align="center" size="1"><br>
+<DIV STYLE="TEXT-ALIGN: CENTER;"><font face="cooper" size="15" color="white"size="5"><BIG>Tienda COMPLEXXX&reg</BIG></DIV>
+<hr align="center" size="1"><br>
+<p align="center"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4487.7663533607565!2d-99.83540433282234!3d16.895908395994358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ca573d923481b9%3A0xc488aad9ad045a3d!2zMTbCsDUzJzQyLjgiTiA5OcKwNTAnMDIuMiJX!5e0!3m2!1ses!2smx!4v1526096185712" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></p>
+
+</BODY>
+</HEAD>
+</HTML>
+
